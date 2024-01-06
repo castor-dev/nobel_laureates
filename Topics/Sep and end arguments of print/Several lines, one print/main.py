10de66bb9a6189efaa73edbe8db9325f296bed12
Carlos Castor <carlos.castor.dev@gmail.com>
2024-01-06 22:19:19 +0100

    commit

--- Topics/Sep and end arguments of print/Several lines, one print/main.py
+++ Topics/Sep and end arguments of print/Several lines, one print/main.py
@@ -4,4 +4,4 @@ line3 = "Return a half a lifetime after – "
 line4 = "All will remain. A scapeless rite."
 
 # your one print() statement here
-print(...)
+print(line1, line2, line3, line4, sep='\n')
